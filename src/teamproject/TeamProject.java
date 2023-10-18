c14745f514528c1993897c18525456c66bbea9bc
Tibong69 <kgb69696969@gmail.com>
2023-10-18 18:29:01 +0900

    주석처리
    어쩔래

--- src/teamproject/TeamProject.java
+++ src/teamproject/TeamProject.java
@@ -19,7 +19,7 @@ public class TeamProject {
     public static void main(String[] args) throws IOException{
         // TODO code application logic here
         IntegrateManager IM = new IntegrateManager();
-        System.out.println("나는 이승환이다 ㅋㅋ 저격당했지");
+        //System.out.println("나는 이승환이다 ㅋㅋ 저격당했지");
         IM.initIM();
         IM.runIM();
         
